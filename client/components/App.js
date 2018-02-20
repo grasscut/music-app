6e3de1906c92f0023202d76bdad5ae9c0e0dfaf5
Olga Orlova <olga.orlova152@gmail.com>
2018-02-21 01:18:59 +0200

    routing for error page added

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import cookie from 'react-cookie';
 import Navigation from '../containers/NavigationContainer';
 import TracksList from '../containers/TracksListContainer';
 import DeletedTracksList from '../containers/DeletedTracksListContainer';
 import LoginButton from './LoginButton';
-import Error from './Error';
 
 class App extends Component {
     componentWillMount() {
@@ -28,7 +27,7 @@ class App extends Component {
                         <Switch>
                             <Route path="/deleted" component={DeletedTracksList} />
                             <Route path="/saved_tracks" component={TracksList} />
-                            <Route path="/error" component={Error} />
+                            <Redirect from="/" to="/saved_tracks" />
                         </Switch>
                     </div>
                 </HashRouter>
--- client/index.js
+++ client/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import './scss/main.scss';
 import musicApp from './reducers';
 import App from './containers/AppContainer';
+import Error from "./components/Error";
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore),
     store = createStoreWithMiddleware(musicApp);
 
 ReactDOM.render(
     <Provider store={store}>
-       <App />
+        <HashRouter>
+            <Switch>
+                <Route path="/error" component={Error} />
+                <Route path="/" component={App} />
+            </Switch>
+        </HashRouter>
     </Provider>,
     document.getElementById('musicAppRoot')
 );
